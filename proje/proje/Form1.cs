e0023c988ce87c4ccd48f620e76b18b4609b2bbf
Murat Berkay Kara <brky.kr_94@hotmail.com>
2018-04-28 13:57:34 +0300

    Initial commit

---
+++ proje/proje/Form1.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proje
+{
+    public partial class Form1 : Form
+    {
+        Bitmap ilk_resim = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Resim Dosyaları|" + "*.bmp;*.jpg;*.gif;*.wmf;*.tif;*.png";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = null;
+                ilk_resim = new Bitmap(Image.FromFile(openFileDialog1.FileName));
+                pictureBox1.Image = ilk_resim;
+                //pictureBox1.Image = Image.FromFile(openFileDialog1.FileName);
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)//boyutlandırma
+
+        {
+            Bitmap boyutlandirma = new Bitmap(ilk_resim, int.Parse(textBox1.Text), int.Parse(textBox2.Text));
+            pictureBox1.Image = boyutlandirma;
+            pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
+        }
+
+        private void button3_Click(object sender, EventArgs e)//tersleme
+        {
+            Bitmap resim = new Bitmap(pictureBox1.Image);
+            Bitmap tersleme = new Bitmap(resim);
+            for (int y = 0; (y <= (resim.Height - 1)); y++)
+             {
+                 for (int x = 0; (x <= (resim.Width - 1)); x++)
+                 {
+                     Color inv = resim.GetPixel(x, y);
+                     inv = Color.FromArgb(255, (255 - inv.R), (255 - inv.G), (255 - inv.B));
+                     tersleme.SetPixel(x, y, inv);
+                 }
+             }
+            pictureBox1.Image = tersleme;
+        }
+
+        private void button4_Click(object sender, EventArgs e)//aynalama sağ
+        {
+            Bitmap resim = new Bitmap(pictureBox1.Image);
+            int width = resim.Width;
+            int height = resim.Height;
+            Bitmap aynalama = new Bitmap(width, height);
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    Color renkler = resim.GetPixel(width - j - 1, i);
+                    aynalama.SetPixel(j, i, renkler);
+                }
+                    
+            }    
+            pictureBox1.Image = aynalama;
+        }
+
+        private void button5_Click(object sender, EventArgs e)//aynalama yukarı
+        {
+            Bitmap resim = new Bitmap(pictureBox1.Image);
+            int width = resim.Width;
+            int height = resim.Height;
+            Bitmap aynalama = new Bitmap(width, height);
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    Color renkler = resim.GetPixel(i, height - j - 1);
+                    aynalama.SetPixel(i, j, renkler);
+                }
+
+            }
+            pictureBox1.Image = aynalama;
+        }
+
+        private void button6_Click(object sender, EventArgs e)//soladöndürme
+        {
+            Bitmap dondur = new Bitmap(pictureBox1.Image);            
+            dondur.RotateFlip(RotateFlipType.Rotate90FlipNone);
+            pictureBox1.Image = dondur;
+
+            /*Bitmap resim = new Bitmap(degisen_resim);
+             Bitmap dondurme = new Bitmap(degisen_resim.Width,degisen_resim.Height);
+             int width = dondurme.Width;
+             int height = dondurme.Height;
+             Graphics g = Graphics.FromImage(dondurme);
+                 g.TranslateTransform((float)width / 2, (float)height / 2); //donus noktası
+                 g.RotateTransform(90);
+                 g.TranslateTransform(-(float)width / 2, -(float)height / 2); //donme noktasını geri yukle
+                 g.DrawImage(resim, new Point(0, 0)); //draw the image on the new bitmap
+
+             pictureBox1.Image = dondurme;
+             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;*/
+
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)//sağadöndürme
+        {
+            Bitmap dondur = new Bitmap(pictureBox1.Image);
+            dondur.RotateFlip(RotateFlipType.Rotate90FlipXY);
+            pictureBox1.Image = dondur;
+
+            /*Bitmap rotatedImage = new Bitmap(ilk_resim);
+            int width = rotatedImage.Width;
+            int height = rotatedImage.Height;
+            using (Graphics g = Graphics.FromImage(rotatedImage))
+            {
+                g.TranslateTransform(width / 2, height / 2); //set the rotation point as the center into the matrix
+                g.RotateTransform(-90); //rotate
+                g.TranslateTransform(-width / 2, -height / 2); //restore rotation point into the matrix
+                g.DrawImage(pictureBox1.Image, new Point(0, 0)); //draw the image on the new bitmap
+            }
+            pictureBox1.Image = null;
+            pictureBox1.Image = rotatedImage;*/
+        }
+
+        private void button8_Click(object sender, EventArgs e)//gritonlama
+        {
+            Bitmap resim = new Bitmap(pictureBox1.Image);
+            Bitmap gri = new Bitmap(resim.Width, resim.Height);
+            int width = gri.Width;
+            int height = gri.Height;
+
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    Color renk = resim.GetPixel(i, j);
+                    int grirenk = (int)((renk.R * 0.3) + (renk.G * 0.59) + (renk.B * 0.11));
+                    Color renk2 = Color.FromArgb(renk.A, grirenk, grirenk, grirenk);
+                    gri.SetPixel(i, j, renk2);
+                }
+            }
+            pictureBox1.Image = gri;
+        }
+
+        private void button9_Click(object sender, EventArgs e)// kırmızı renk kanalı
+        {
+            Bitmap resim = new Bitmap(pictureBox1.Image);
+            Bitmap renklendirme= new Bitmap(resim.Width,resim.Height);
+            int width = renklendirme.Width;
+            int height = renklendirme.Height;
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    Color renkler = resim.GetPixel(i, j);
+                    Color kanal = Color.FromArgb(renkler.R, 0, 0);
+                    renklendirme.SetPixel(i, j, kanal);
+                }
+            }
+            pictureBox1.Image = renklendirme;
+           // pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void button10_Click(object sender, EventArgs e)//Yeşil Renk kanalı
+        {
+            Bitmap resim = new Bitmap(pictureBox1.Image);
+            Bitmap renklendirme = new Bitmap(resim.Width, resim.Height);
+            int width = renklendirme.Width;
+            int height = renklendirme.Height;
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    Color renkler = resim.GetPixel(i, j);
+                    Color kanal = Color.FromArgb(0, renkler.G, 0);
+                    renklendirme.SetPixel(i, j, kanal);
+                }
+            }
+            pictureBox1.Image = renklendirme;
+            //pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void button11_Click(object sender, EventArgs e)//Mavi renk kanalı
+        {
+            Bitmap resim = new Bitmap(pictureBox1.Image);
+            Bitmap renklendirme = new Bitmap(resim.Width, resim.Height);
+            int width = renklendirme.Width;
+            int height = renklendirme.Height;
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    Color renkler = resim.GetPixel(i, j);
+                    Color kanal = Color.FromArgb(0, 0, renkler.B);
+                    renklendirme.SetPixel(i, j, kanal);
+                }
+            }
+            pictureBox1.Image = renklendirme;
+            //pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void button12_Click(object sender, EventArgs e)//sıfırlama
+        {
+            Bitmap resim = new Bitmap(ilk_resim);
+            pictureBox1.Image = null;
+            pictureBox1.Image = resim;
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void button13_Click(object sender, EventArgs e)//kaydetme
+        {           
+            kaydetme.Filter = "JPG images (*.jpg)|*.jpg";
+            if (kaydetme.ShowDialog() == DialogResult.OK)
+            {           
+                Bitmap bmp = new Bitmap(pictureBox1.Image);                
+                bmp.Save(kaydetme.FileName);
+            }
+        }
+        
+        private void button14_Click(object sender, EventArgs e)//histogram
+        {
+            Bitmap resim = new Bitmap(pictureBox1.Image);//tüm piksellerdeki renkleri alır ve renkleri diziye atar
+            int width = resim.Width;
+            int height = resim.Height;
+            int[,] kirmizi = new int[width,height];
+            int[,] yesil = new int[width, height];
+            int[,] mavi = new int[width, height];
+            int[,] gri = new int[width, height];
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    Color renkler = resim.GetPixel(i, j);
+                    kirmizi[i,j] = (int)renkler.R;
+                    yesil[i,j] = (int)renkler.G;
+                    mavi[i,j] = (int)renkler.B;
+                    gri[i,j] = (int)((renkler.R * 0.3) + (renkler.G * 0.59) + (renkler.B * 0.11));
+                }
+            }
+          
+            Form2 goster = new Form2();
+            for (int i = 0; i < width; i=i+20)
+            {
+                for (int j = 0; j < height; j=j+20)//diziye atılan renkleri grafige aktarır
+                {
+                    goster.grafik1.Series["Red"].Points.AddY(kirmizi[i,j]);
+                    goster.grafik2.Series["Green"].Points.AddY(yesil[i, j]);
+                    goster.grafik3.Series["Blue"].Points.AddY(mavi[i, j]);
+                    goster.grafik4.Series["Grey"].Points.AddY(gri[i, j]);
+                }
+            }
+
+
+            goster.Show();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ proje/proje/Form2.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace proje
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.grafik1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.grafik2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.grafik3 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.grafik4 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.grafik1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grafik2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grafik3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grafik4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grafik1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.grafik1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.grafik1.Legends.Add(legend1);
+            this.grafik1.Location = new System.Drawing.Point(25, 26);
+            this.grafik1.Name = "grafik1";
+            series1.BorderColor = System.Drawing.Color.Red;
+            series1.BorderWidth = 3;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Area;
+            series1.Color = System.Drawing.Color.Red;
+            series1.Legend = "Legend1";
+            series1.Name = "Red";
+            this.grafik1.Series.Add(series1);
+            this.grafik1.Size = new System.Drawing.Size(425, 190);
+            this.grafik1.TabIndex = 0;
+            this.grafik1.Text = "chart1";
+            // 
+            // grafik2
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.grafik2.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.grafik2.Legends.Add(legend2);
+            this.grafik2.Location = new System.Drawing.Point(456, 26);
+            this.grafik2.Name = "grafik2";
+            series2.BorderWidth = 3;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Area;
+            series2.Color = System.Drawing.Color.Green;
+            series2.Legend = "Legend1";
+            series2.Name = "Green";
+            this.grafik2.Series.Add(series2);
+            this.grafik2.Size = new System.Drawing.Size(425, 190);
+            this.grafik2.TabIndex = 1;
+            this.grafik2.Text = "chart2";
+            // 
+            // grafik3
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.grafik3.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.grafik3.Legends.Add(legend3);
+            this.grafik3.Location = new System.Drawing.Point(25, 222);
+            this.grafik3.Name = "grafik3";
+            series3.BorderWidth = 3;
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Area;
+            series3.Legend = "Legend1";
+            series3.Name = "Blue";
+            this.grafik3.Series.Add(series3);
+            this.grafik3.Size = new System.Drawing.Size(425, 190);
+            this.grafik3.TabIndex = 2;
+            this.grafik3.Text = "chart3";
+            // 
+            // grafik4
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.grafik4.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.grafik4.Legends.Add(legend4);
+            this.grafik4.Location = new System.Drawing.Point(456, 222);
+            this.grafik4.Name = "grafik4";
+            series4.BorderWidth = 3;
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Area;
+            series4.Color = System.Drawing.Color.Gray;
+            series4.Legend = "Legend1";
+            series4.Name = "Grey";
+            this.grafik4.Series.Add(series4);
+            this.grafik4.Size = new System.Drawing.Size(425, 190);
+            this.grafik4.TabIndex = 3;
+            this.grafik4.Text = "chart4";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(894, 421);
+            this.Controls.Add(this.grafik4);
+            this.Controls.Add(this.grafik3);
+            this.Controls.Add(this.grafik2);
+            this.Controls.Add(this.grafik1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grafik1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grafik2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grafik3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grafik4)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        public System.Windows.Forms.DataVisualization.Charting.Chart grafik1;
+        public System.Windows.Forms.DataVisualization.Charting.Chart grafik2;
+        public System.Windows.Forms.DataVisualization.Charting.Chart grafik3;
+        public System.Windows.Forms.DataVisualization.Charting.Chart grafik4;
+    }
+}
